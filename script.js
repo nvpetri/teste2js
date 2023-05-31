d196dda23728c24239c9f43c83eadd75396ab54c
Nicolas Petri <123608034+nvpetri@users.noreply.github.com>
2023-05-31 20:36:48 -0300

    fim

---
+++ script.js
@@ -0,0 +1,16 @@
+function redirectToIndex2() {
+    window.location.href = "index2.html";
+}
+
+function rejectAnswer() {
+    var button = document.getElementsByClassName("button")[1];
+    button.style.position = "absolute";
+    setInterval(function() {
+        var position = button.style.left;
+        var newPosition = parseInt(position) + 10;
+        button.style.left = newPosition + "px";
+        if (newPosition >= window.innerWidth - button.offsetWidth) {
+            button.style.left = "0px";
+        }
+    }, 100);
+}
